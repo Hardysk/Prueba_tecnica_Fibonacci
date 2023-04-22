37948f2a158ed8d816cdcf4bda85f5b815e7dba6
Hardysk <decas.0708@gmail.com>
2023-04-22 12:26:14 -0500

    Se implemento el desarrollo para prueba tecnica usando Python v3

---
+++ prueba_tecnica.py
@@ -0,0 +1,16 @@
+def serie():
+    valor = int (input("Introduce numero a calcular: "))
+    resultado = fibonacci (valor)
+    print ("Su resultado en la serie Fibonacci es: " + str (resultado))
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        for i in range (1,n): 
+            return (fibonacci(n-1) + fibonacci(n-2))
+
+
+if __name__ == "__main__":
+    serie ()
+    
